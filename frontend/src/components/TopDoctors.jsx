15df20b24455ca7a30c2d047375c480dc0282ba0
dheeraj-saini-git <149778591+dheeraj-saini-git@users.noreply.github.com>
2025-08-19 20:43:31 +0530

    Update TopDoctors.jsx

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,37 +1,58 @@
-import React, {useContext} from 'react';
-import { useNavigate } from 'react-router-dom';
-import { AppContext } from '../context/AppContext';
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
-export default function TopDoctors(){
-    const navigate = useNavigate() ;
-    const {doctors} = useContext(AppContext);
-    return (
-        <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
-            <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
-            <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors</p>
-            <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
-                {
-                    doctors.slice(0,10).map((item,index)=>(
-                        <div
-                        key={index}
-                        onClick={()=>navigate(`appointment/${item._id}`)}
-                         className='border border-blue-200 rounded-x1 overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
-                          <img className='bg-blue-50' src={item.image} alt="" />
-                          <div className='p-4'>
-                            <div className={`flex items-center gap-2 text-sm text-center ${item.available? 'text-green-500' : 'text-gray-500' }`}>
-                                <p className={`w-2 h-2 ${item.available? 'bg-green-500' : 'bg-gray-500' } rounded-full`}></p><p> {item.available ? 'Available' : 'Not Available' }</p>
-                            </div>
-                            <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
-                            <p className='text-gray-600 text-sm'>{item.speciality}</p>
-                          </div>
-                        </div>
-                       
-                    ))
-                }
+export default function TopDoctors() {
+  const navigate = useNavigate();
+  const { doctors, loading } = useContext(AppContext);
+
+  return (
+    <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
+      <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
+      <p className="sm:w-1/3 text-center text-sm">
+        Simply browse through our extensive list of trusted doctors
+      </p>
+      { loading ? (
+        <div className="flex justify-center items-center h-3/4">
+            <div className="animate-spin rounded-full h-11 w-11 border-3 border-purple-500 border-t-transparent">
             </div>
-            <button 
-            onClick={()=>(navigate(`/doctors`), scrollTo(0,0))}
-            className='bg-blue-100 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
-        </div>
-    )
+          </div>
+      ) : (
+        
+        <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
+        {doctors.slice(0, 10).map((item, index) => (
+          <div
+            key={index}
+            onClick={() => navigate(`appointment/${item._id}`)}
+            className="border border-blue-200 rounded-x1 overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+          >
+            <img className="bg-blue-50" src={item.image} alt="" />
+            <div className="p-4">
+              <div
+                className={`flex items-center gap-2 text-sm text-center ${
+                  item.available ? "text-green-500" : "text-gray-500"
+                }`}
+              >
+                <p
+                  className={`w-2 h-2 ${
+                    item.available ? "bg-green-500" : "bg-gray-500"
+                  } rounded-full`}
+                ></p>
+                <p> {item.available ? "Available" : "Not Available"}</p>
+              </div>
+              <p className="text-gray-900 text-lg font-medium">{item.name}</p>
+              <p className="text-gray-600 text-sm">{item.speciality}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    )}
+      <button
+      onClick={() => (navigate(`/doctors`), scrollTo(0, 0))}
+      className="bg-blue-100 text-gray-600 px-12 py-3 rounded-full mt-10"
+      >
+        more
+      </button>
+    </div>
+  );
 }
