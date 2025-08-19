e7f5275c5e49120d129bd01fee46a0dd4d829da3
dheeraj-saini-git <149778591+dheeraj-saini-git@users.noreply.github.com>
2025-08-19 17:02:10 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 
 export default function AppContextProvider(props) {
   const currencySymbol = "$";
-  const backendUrl = "https://appointment-4op9.onrender.com/";
+  const backendUrl = "appointment-server-puce.vercel.app/";
 
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(
